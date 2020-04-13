80031c7470317bbf45e4afbbbf5845e7fcda2cc2
xacrys <cristiantintinmedina@hotmail.com>
2020-04-12 23:00:00 -0500

    Cambios sobre el aplicativo

--- facturacionpy/facturacionpy/settings.py
+++ facturacionpy/facturacionpy/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'facturacionpy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'facturacion',
+        'USER': 'cris',
+        'PASSWORD': 'RivadeneiraAA25',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -103,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-EC'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Bogota'
 
 USE_I18N = True
 
